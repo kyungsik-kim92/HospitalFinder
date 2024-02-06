c440e86ba8192eb44e9ccbdf1bcbce3c0598456e
Kim Kyungsik <ksiki1@naver.com>
2024-02-06 18:00:36 +0900

    맵뷰 띄우기 실물 기기에서는 화면이 나옴

--- app/src/main/java/com/example/hospitalfinder/MainActivity.kt
+++ app/src/main/java/com/example/hospitalfinder/MainActivity.kt
@@ -1,19 +1,30 @@
 package com.example.hospitalfinder
 
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.content.pm.Signature
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
 import com.example.hospitalfinder.databinding.ActivityMainBinding
 import net.daum.mf.map.api.MapView
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-
+        val mapView = MapView(this)
+        val mapViewContainer = binding.mapView
+        mapViewContainer.addView(mapView)
 
     }
+
+
 }
